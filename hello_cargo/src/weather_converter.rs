fba441c6d802badc173caba17e5b118ed3dc0c5e
rio-cl <147709250+rio-cl@users.noreply.github.com>
2025-10-09 22:05:57 +0100

    Initial commit

---
+++ hello_cargo/src/weather_converter.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let temp1 = 36.5;
+    let temp2 = 97.7 ;
+    let converter = 33.8;
+
+    let f1 = temp1 * converter;
+    let f2 = temp2 * converter;
+
+    println!("{}'C = {}'F", temp1, f1);
+    println!("{}'C = {}'F", temp2, f2);
+}
